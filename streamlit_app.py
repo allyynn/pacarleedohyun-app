29502f8bdba34937385e00724a626bcfd7cb7b72
allyynn <azrinaghalin19@gmail.com>
2025-05-21 15:13:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
  "mau punya pacar kaya sunjae"
 )
 st.image("IMG_20250414_210659_653.webp")
+
+st.title("math")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+   st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
