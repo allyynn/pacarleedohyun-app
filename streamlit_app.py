e930299051b22c0d9de53f8cce39ef168cbf92c6
allyynn <azrinaghalin19@gmail.com>
2025-05-21 15:13:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write(
 )
 st.image("IMG_20250414_210659_653.webp")
 
-st.title("math")
+st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
 
